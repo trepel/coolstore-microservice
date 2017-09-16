fe142da46f2be67cbf4d27830fea34cd628e41f7
trepel <trepel@redhat.com>
2017-09-16 11:47:47 +0200

    Enabled CORS for Cart Service

--- cart-service/src/main/java/com/redhat/coolstore/rest/JerseyConfig.java
+++ cart-service/src/main/java/com/redhat/coolstore/rest/JerseyConfig.java
@@ -2,10 +2,12 @@ package com.redhat.coolstore.rest;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 
 @Component
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
 		register(CartEndpoint.class);
+	        register(CorsFilter.class);
 	}
 }
